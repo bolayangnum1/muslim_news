5437b0fa0f760469adf3d94c88bf36361da12f21
bolayangnum1 <bolot.muxa@gmail.com>
2022-03-31 16:35:05 +0600

    bugfix22

--- main/settings.py
+++ main/settings.py
@@ -74,23 +74,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main.wsgi.application'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'daqv0io2efdfe7',
-#         'USER': 'clzaxzyucwfjcm',
-#         'PASSWORD': '1b20449a267da5008edc5cf1d31e1ab873cd733a62e3bd3df2930fe04618bdad',
-#         'HOST': 'ec2-52-214-23-110.eu-west-1.compute.amazonaws.com',
-#         'PORT': 5432
-#     }
-# }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'daqv0io2efdfe7',
+        'USER': 'clzaxzyucwfjcm',
+        'PASSWORD': '1b20449a267da5008edc5cf1d31e1ab873cd733a62e3bd3df2930fe04618bdad',
+        'HOST': 'ec2-52-214-23-110.eu-west-1.compute.amazonaws.com',
+        'PORT': 5432
+    }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 
 AUTH_PWD_MODULE = 'django.contrib.auth.password_validation.'
--- vacancies/models.py
+++ vacancies/models.py
@@ -2,24 +2,6 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class Registration(models.Model):
-
-    class Meta:
-        verbose_name = "данные пользователя"
-        verbose_name_plural = "данные пользователей"
-
-    phone_number = PhoneNumberField(unique=True, verbose_name='Номер телефона')
-    email = models.EmailField(unique=True, verbose_name='Электронная почта')
-    fio = models.CharField(verbose_name='ФИО', max_length=200)
-    dateofbirth = models.DateField(verbose_name='Дата рождения')
-    city = models.CharField(verbose_name='Город', max_length=100)
-    workexperience = models.CharField(verbose_name='Стаж работы', max_length=255)
-    levelstudies = models.CharField(verbose_name='Уровень образования', max_length=100)
-
-    def __str__(self):
-        return self.fio
-
-
 class Vacancy(models.Model):
     class Meta:
         verbose_name = "Вакансия"
@@ -32,11 +14,27 @@ class Vacancy(models.Model):
     company = models.CharField(verbose_name="Компания", max_length=500)
     requirements = models.TextField(verbose_name="Требования", max_length=1000)
     salary = models.DecimalField(verbose_name="Зарплата", decimal_places=2, max_digits=100)
+    adress = models.CharField('Адрес', max_length=250)
+    time_work = models.CharField('рабочее время', max_length=200)
+    number = models.CharField('Номер телефона', max_length=100)
+    education = models.TextField('образование')
     duties = models.TextField(verbose_name="Обязанности")
-    conditions = models.TextField(verbose_name="Условия для религиозных", max_length=1000)
-    registration = models.ForeignKey(Registration, on_delete=models.CASCADE, verbose_name='Вакансия')
+    conditions = models.TextField(verbose_name="Условия работы", max_length=1000)
 
-#адрес
-#время работы
-#номер телефона
-#образование
+
+class Registration(models.Model):
+    class Meta:
+        verbose_name = "данные пользователя"
+        verbose_name_plural = "данные пользователей"
+
+    phone_number = PhoneNumberField(unique=True, verbose_name='Номер телефона')
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
+    fio = models.CharField(verbose_name='ФИО', max_length=200)
+    dateofbirth = models.DateField(verbose_name='Дата рождения')
+    city = models.CharField(verbose_name='Город', max_length=100)
+    workexperience = models.CharField(verbose_name='Стаж работы', max_length=255)
+    levelstudies = models.CharField(verbose_name='Уровень образования', max_length=100)
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, verbose_name='Заявка')
+
+    def __str__(self):
+        return self.fio
