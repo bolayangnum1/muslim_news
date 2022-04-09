f37dbfdec552d446e38b569e3d973e3c90cbb537
bolayangnum1 <bolot.muxa@gmail.com>
2022-04-09 12:33:59 +0600

    Bolot

--- addresses/models.py
+++ addresses/models.py
@@ -24,9 +24,9 @@ class BaseAbstractCategory(models.Model):
 
     address = models.CharField(verbose_name="Адрес", max_length=500)
     contacts = models.CharField(verbose_name="Контакты", max_length=24)
-    images = models.ImageField(verbose_name="Фотографии", upload_to='files/images/')
-    longitude = models.FloatField(verbose_name='Долгота', blank=True)
-    latitude = models.FloatField(verbose_name='Широта', blank=True)
+    images = models.ImageField(verbose_name="Фотографии", upload_to='media')
+    longitude = models.FloatField(verbose_name='Долгота', blank=True, null=True)
+    latitude = models.FloatField(verbose_name='Широта', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
 
